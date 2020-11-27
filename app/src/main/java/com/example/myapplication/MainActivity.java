d1d5ade2eb68a2460d9628a7e74a1293bc49b43c
Ovsyannikov_M <maksim.ov@mail.ru>
2020-11-28 00:46:24 +0300

    fix CL

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,8 +16,6 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
 
-    protected static final String MainScreenFragmentTAG = "MainScreenFragmentTAG";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
-                    .add(R.id.fragment_container, new MainScreenFragment(), MainScreenFragmentTAG)
+                    .add(R.id.fragment_container, new MainScreenFragment())
                     .commit();
         }
     }
