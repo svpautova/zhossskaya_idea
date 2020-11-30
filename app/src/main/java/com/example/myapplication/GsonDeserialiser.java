538980330ec3709de944266fe01894695181d76f
valeksukhin <a248@bk.ru>
2020-11-30 22:25:23 +0300

    api working process

--- app/src/main/java/com/example/myapplication/GsonDeserialiser.java
+++ app/src/main/java/com/example/myapplication/GsonDeserialiser.java
@@ -14,14 +14,14 @@ public class GsonDeserialiser implements JsonDeserializer<List<Photo>> {
     List<Photo> photos = new ArrayList<Photo>();
     @Override
     public List<Photo> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        JsonArray photos_j = json.getAsJsonObject().get("photos").getAsJsonArray();
+        JsonArray photosJ = json.getAsJsonObject().get("photos").getAsJsonArray();
 
-        for (int i=0; i < photos_j.size(); i++) {
-            int id = photos_j.get(i).getAsJsonObject().get("id").getAsInt();
-            String url = photos_j.get(i).getAsJsonObject().get("url").getAsString();
-            String large = photos_j.get(i).getAsJsonObject().get("src").getAsJsonObject().get("large").getAsString();
-            String large2x = photos_j.get(i).getAsJsonObject().get("src").getAsJsonObject().get("large2x").getAsString();
-            String medium = photos_j.get(i).getAsJsonObject().get("src").getAsJsonObject().get("medium").getAsString();
+        for (int i=0; i < photosJ.size(); i++) {
+            int id = photosJ.get(i).getAsJsonObject().get("id").getAsInt();
+            String url = photosJ.get(i).getAsJsonObject().get("url").getAsString();
+            String large = photosJ.get(i).getAsJsonObject().get("src").getAsJsonObject().get("large").getAsString();
+            String large2x = photosJ.get(i).getAsJsonObject().get("src").getAsJsonObject().get("large2x").getAsString();
+            String medium = photosJ.get(i).getAsJsonObject().get("src").getAsJsonObject().get("medium").getAsString();
 
             Photo currentPhoto = new Photo(id, url, large, large2x, medium);
 
