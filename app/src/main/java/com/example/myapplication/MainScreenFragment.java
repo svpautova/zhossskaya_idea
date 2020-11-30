ca235bc3a6103cd724d137f83fd36ce1f7806aab
valeksukhin <a248@bk.ru>
2020-11-30 22:29:20 +0300

    api working process

--- app/src/main/java/com/example/myapplication/MainScreenFragment.java
+++ app/src/main/java/com/example/myapplication/MainScreenFragment.java
@@ -29,34 +29,25 @@ public class MainScreenFragment extends Fragment implements View.OnClickListener
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_mainscreen, container, false);
         imageView = v.findViewById(R.id.image_View);
-      
         likeButton = v.findViewById(R.id.like_button);
         likeButton.setOnClickListener(this);
-      
         dislikeButton = v.findViewById(R.id.dislike_button);
         dislikeButton.setOnClickListener(this);
-      
         GetPhotos.getPhotos(v);
         return v;
-
     }
 
 
     @Override
     public void onClick(View v) {
-        
         if (v.getId()==R.id.like_button){
-
             Buttons.Like_button();
             Log.d("!!!!!!", "click OK");
         }
         if (v.getId()==R.id.dislike_button){
-
             Buttons.Dislike_button();
             Log.d("!!!!!!", "click decline");
         }
-
     }
-
 }
 
