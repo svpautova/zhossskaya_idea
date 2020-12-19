0596825ae94f57f9366e43d2d2ffa09364a8e33c
Maksim Ovsyannikov <60704312+OvsyannikovMaksim@users.noreply.github.com>
2020-12-20 01:12:39 +0300

    Update AppDatabase.java

--- app/src/main/java/com/example/myapplication/AppDatabase.java
+++ app/src/main/java/com/example/myapplication/AppDatabase.java
@@ -4,7 +4,6 @@ package com.example.myapplication;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-import java.util.List;
 
 @Database(entities = {ImageFile.class /*, AnotherEntityType.class, AThirdEntityType.class */}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
