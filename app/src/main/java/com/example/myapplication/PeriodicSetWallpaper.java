e4230356f28f16381f9b6c9016c18528cc0ca060
Maksim Ovsyannikov <60704312+OvsyannikovMaksim@users.noreply.github.com>
2020-12-20 00:51:47 +0300

    Update PeriodicSetWallpaper.java

--- app/src/main/java/com/example/myapplication/PeriodicSetWallpaper.java
+++ app/src/main/java/com/example/myapplication/PeriodicSetWallpaper.java
@@ -4,18 +4,11 @@ import android.app.WallpaperManager;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 
 
